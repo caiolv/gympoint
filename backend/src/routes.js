d5f610ba2c12e7d025e7f8313adb896dbc0446cd
Caio Oliveira <caiolv@me.com>
2019-10-28 01:08:11 -0300

    Validar dados de entrada da autenticação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,16 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 
+import autoMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(autoMiddleware);
+
+routes.get('/students', (req, res) => {
+  return res.json({ ok: true, user_id: req.userId });
+})
 
 export default routes;
